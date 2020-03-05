11d32393b549cdfdf6468e2e6fd35a2a28b7aa85
seokkjj83 <58294616+seokkjj83@users.noreply.github.com>
2020-03-05 17:38:54 +0100

    Add files via upload
    Ejemplo de base de datos

---
+++ data.php
@@ -0,0 +1,25 @@
+<?php 
+$biblioteca=[
+'LA_ULTIMA_DAGA' => [
+    'AUTOR' => 'Jacinto Arán Abelló',
+    'EDITORIAL' => 'TREGOLAM LITERATURA S.L.',
+    'PAGINAS' => 188,
+    'GENERO' => 'Ciencia Ficción',
+    'FECHA' => 01-02-2016,
+    'ISBN' => 978-84-944474-4-0,
+    'ORIGEN' => 'ESPAÑA',
+    'LINK' => 'https://reubenbooks.co.uk/asd12d12dsaasd/descargar/La+daga.pdf'
+    ],							
+'LA_ULTIMA_DAGA' => [
+    'AUTOR' => 'Jacinto Arán Abelló',
+    'EDITORIAL' => 'TREGOLAM LITERATURA S.L.',
+    'PAGINAS' => 188,
+    'GENERO' => 'Ciencia Ficción',
+    'FECHA' => 01-02-2016,
+    'ISBN' => 978-84-944474-4-0,
+    'ORIGEN' => 'ESPAÑA',
+    'LINK' => 'https://reubenbooks.co.uk/asd12d12dsaasd/descargar/La+daga.pdf'
+    ]
+];	
+
+?>
